49daae4bd9f94fbbd07f508b47b49a7243ebac54
Rahmat Azad <59721909+rhmtazad@users.noreply.github.com>
2021-04-14 00:47:52 +0430

    Added the new user interface
    This user interface offers new features that are more aligned with what we expected from this project when we first decided to work on. Also, it is more user-friendly and ergonomic. The user interface has a minimalistic design that offers necessary functionalities without too much cognitive overload for the user. The reasons behind this approach was more or less these articles:
    https://en.wikipedia.org/wiki/Chunking_(psychology)
    https://en.wikipedia.org/wiki/The_Magical_Number_Seven,_Plus_or_Minus_Two

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
-if __name__ == "__main__":
-    pass
+import sys
+
+from PyQt5 import QtWidgets
+from PyQt5.uic import loadUiType
+
+main_window, _ = loadUiType('main.ui')
+
+
+class MainApp(QtWidgets.QMainWindow, main_window):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        self.setupUi(self)
+
+    def center_window(self):
+        frame_geometry = self.frameGeometry()
+        center_point = QtWidgets.QDesktopWidget().availableGeometry().center()
+        frame_geometry.moveCenter(center_point)
+        self.move(frame_geometry.topLeft())
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainApp()
+    window.show()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    main()
