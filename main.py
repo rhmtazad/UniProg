53a240050b34dba52966c422f833be1562b2821e
msrezaie <79583632+msrezaie@users.noreply.github.com>
2021-04-17 02:21:14 +0430

    page navigation implemented, object names identified

--- main.py
+++ main.py
@@ -1,149 +1,3 @@
-<<<<<<< HEAD
-from PyQt5.QtWidgets import *
-from PyQt5.uic import loadUiType
-import sqlite3
-import sys
-import ctypes
-
-main_window, _ = loadUiType('ui/main.ui')
-
-
-# main application class
-class MainApp(QMainWindow, main_window):
-    def __init__(self):
-        # call the parent constructor
-        QMainWindow.__init__(self)
-
-        # setup the user interface
-        self.setupUi(self)
-
-        # center the window
-        self.center_window()
-
-        # handle button actions
-        self.button_handler()
-
-        # handle switch between pages
-        self.page_handler()
-
-        # make sure the login page is default
-        self.logout()
-
-    # position application window in center
-    def center_window(self):
-        # geometry of the main window
-        frame_geometry = self.frameGeometry()
-
-        # center point of screen
-        center_point = QDesktopWidget().availableGeometry().center()
-
-        # move rectangle's center point to screen's center point
-        frame_geometry.moveCenter(center_point)
-
-        # top left of rectangle becomes top left of window centering it
-        self.move(frame_geometry.topLeft())
-
-    # switch between pages
-    def page_handler(self):
-        # student portal page
-        self.studentPortalBtn.clicked.connect(lambda: self.page('StudentPortal'))
-        self.stdPortalDBBtn.clicked.connect(lambda: self.page('stdPortalDashboardView'))
-        self.stdPortalAddInfoBtn.clicked.connect(lambda: self.page('stdPortalDashboardModify'))
-
-        # degreePlan portal page
-        self.degreePlanPortalBtn.clicked.connect(lambda: self.page('DegreePlanPortal'))
-        self.dpPortalDBBtn.clicked.connect(lambda: self.page('dashboardCreditsView'))
-        self.dpPortalAddCoursesBtn.clicked.connect(lambda: self.page('AddCourse'))
-        self.dpPortalDPViewBtn.clicked.connect(lambda: self.page('DegreePlan'))
-        self.dpPortalCoursesViewBtn.clicked.connect(lambda: self.page('ViewCourse'))
-
-        # go to home page
-        self.stdPortalHomeBtn.clicked.connect(lambda: self.page('MainPage'))
-        self.dpPortalHomeBtn.clicked.connect(lambda: self.page('MainPage'))
-
-        # add student page
-        self.rgstrBtn.clicked.connect(lambda: self.page('RegisterPage'))
-
-    # handle actions for each button
-    def button_handler(self):
-        # login when button is pressed
-        self.lgnBtn.clicked.connect(self.login)
-
-        # logout when button is pressed
-        self.lgtBtnMainPage.clicked.connect(self.logout)
-        self.stdPortalLgtBtn.clicked.connect(self.logout)
-        self.dpPortalLgtBtn.clicked.connect(self.logout)
-
-        # back button
-        self.backBtnRegisterPage.clicked.connect(self.logout)
-
-    def page(self, name):
-        if name == 'MainPage':
-            self.mainWindows.setCurrentWidget(self.MainPage)
-        elif name == 'LoginPage':
-            self.mainWindows.setCurrentWidget(self.LoginPage)
-        elif name == 'RegisterPage':
-            self.mainWindows.setCurrentWidget(self.RegisterPage)
-        elif name == 'StudentPortal':
-            self.mainWindows.setCurrentWidget(self.StudentPortal)
-            self.studentWindows.setCurrentWidget(self.stdPortalDashboardView)
-        elif name == 'stdPortalDashboardModify':
-            self.studentWindows.setCurrentWidget(self.stdPortalDashboardModify)
-        elif name == 'stdPortalDashboardView':
-            self.studentWindows.setCurrentWidget(self.stdPortalDashboardView)
-        elif name == 'DegreePlanPortal':
-            self.mainWindows.setCurrentWidget(self.DegreePlanPortal)
-            self.dPlanWindows.setCurrentWidget(self.dashboardCreditsView)
-        elif name == 'dashboardCreditsView':
-            self.dPlanWindows.setCurrentWidget(self.dashboardCreditsView)
-        elif name == 'AddCourse':
-            self.dPlanWindows.setCurrentWidget(self.AddCourse)
-        elif name == 'DegreePlan':
-            self.dPlanWindows.setCurrentWidget(self.DegreePlan)
-        elif name == 'ViewCourse':
-            self.dPlanWindows.setCurrentWidget(self.ViewCourse)
-        else:
-            print('wrong input name or object name')
-
-    def login(self):
-        entered_username = self.unameInput.text()
-        entered_password = self.passInput.text()
-
-        # get username and password form database
-
-        # evaluate input username and password
-        if entered_username == "admin" and entered_password == "admin":
-            # set home page as current page
-            self.page('MainPage')
-
-            # clear the username and password input fields
-            self.unameInput.clear()
-            self.passInput.clear()
-        else:
-            # clear the input fields and say try again
-            self.unameInput.clear()
-            self.passInput.clear()
-            ctypes.windll.user32.MessageBoxW(None, "Incorrect Credentials", "Error!", 0)
-
-    def logout(self):
-        # clear the input fields and say try again
-        self.unameInput.clear()
-        self.passInput.clear()
-
-        # set login page as current page
-        self.page('LoginPage')
-
-
-def main():
-    app = QApplication(sys.argv)
-    window = MainApp()
-    window.show()
-    app.exec_()
-
-
-if __name__ == '__main__':
-    main()
-=======
 import sys
 
 from PyQt5 import uic
@@ -159,6 +13,38 @@ class UserInterface(QMainWindow):
         self.setAttribute(Qt.WA_TranslucentBackground, True)
         uic.loadUi('ui/main.ui', self)
         self.center_window()
+        self.page('dashboard_page')
+
+        # handle button actions
+        self.button_handler()
+
+        # handle switch between pages
+        self.page_handler()
+
+    # switch between pages
+    def page_handler(self):
+        # pages navigation
+        self.dashboard.clicked.connect(lambda: self.page('dashboard_page'))
+        self.degreeplan.clicked.connect(lambda: self.page('degreeplan_page'))
+        self.courses.clicked.connect(lambda: self.page('courses_page'))
+        self.account.clicked.connect(lambda: self.page('account_page'))
+
+    # handle actions for each button
+    def button_handler(self):
+        self.closeBtn.clicked.connect(self.close)
+        self.minimizeBtn.clicked.connect(self.showMinimized)
+
+    def page(self, name):
+        if name == 'dashboard_page':
+            self.page_manager.setCurrentWidget(self.dashboard_page)
+        elif name == 'degreeplan_page':
+            self.page_manager.setCurrentWidget(self.degreeplan_page)
+        elif name == 'courses_page':
+            self.page_manager.setCurrentWidget(self.courses_page)
+        elif name == 'account_page':
+            self.page_manager.setCurrentWidget(self.account_page)
+        else:
+            print('wrong input name or object name')
 
     def center_window(self):
         frame_geometry = self.frameGeometry()
@@ -166,9 +52,135 @@ class UserInterface(QMainWindow):
         frame_geometry.moveCenter(center_point)
         self.move(frame_geometry.topLeft())
 
+    # For Values of Dashboard page
+    def dashboardPage(self):
+        # show major
+        self.user_sum_major.setText()
+        # show concentration
+        self.user_sum_con.setText()
+
+        # show course category summery
+        self.sum_general_com.setText()
+        self.sum_general_rem.setText()
+        self.sum_general_cre.setText()
+
+        self.sum_core_com.setText()
+        self.sum_core_rem.setText()
+        self.sum_core_cre.setText()
+
+        self.sum_selective_com.setText()
+        self.sum_selective_rem.setText()
+        self.sum_selective_cre.setText()
+
+        self.sum_concent_com.setText()
+        self.sum_concent_rem.setText()
+        self.sum_concent_cre.setText()
+
+        # degree plan summery
+        self.sum_year.setText()
+        self.sum_TotCredit.setText()
+
+        # CGPA summery
+        self.sum_coursesCom.setText()
+        self.sum_CGPA.setText()
+
+        # graduation summery
+        self.sum_coursesCom.setText()
+        self.sum_CGPA.setText()
+
+        # estimate time
+        self.sum_remSem2Grad.setText()
+
+    # For Values of Degree Plan page
+    def degreePlanPage(self):
+        # show current degree plan
+        self.dp_major.setText()
+        self.dp_year.setText()
+        self.dp_courses.setText()
+        self.dp_credits.setText()
+
+        # show Total Courses and Credits by Category
+        self.dp_gen_courses.setText()
+        self.dp_gen_credits.setText()
+        self.dp_cor_courses.setText()
+        self.dp_cor_credits.setText()
+        self.dp_con_courses.setText()
+        self.dp_con_credits.setText()
+
+        # Configure Your Degree Plan
+        self.config_degreeplan_major.text()
+        self.config_degreeplan_year.text()
+        self.config_degreeplan_concentration.text()
+        self.config_degreeplan_credit.text()
+        self.config_degreeplan_general.text()
+        self.config_degreeplan_core.text()
+        self.config_degreeplan_selective.text()
+        self.config_degreeplan_concentration_credit.text()
+
+        # buttons
+        self.config_degreeplan_reset.clicked.connect()
+        self.config_degreeplan_save.clicked.connect()
+
+    # view data in table
+    @staticmethod
+    def view(widget_name):
+        widget_name.setRowCount(0)
+
+        for row_index, row_data in enumerate(db.cursor.fetchall()):
+            widget_name.insertRow(row_index)
+            for column_index, column_data in enumerate(row_data):
+                widget_name.setItem(row_index, column_index, QTableWidgetItem(str(column_data)))
+
+    def coursesPage(self):
+        # Add Courses
+        self.course_input_ID.text()
+        self.course_input_name.text()
+        self.course_input_category.text()
+        self.course_input_grade.text()
+        self.course_input_credits.text()
+        # buttons
+        self.course_input_save.clicked.connect()
+        self.course_input_reset.clicked.connect()
+
+        # Edit/Delete Courses
+        self.course_modify_search.text()
+        # buttons: table ObjectName (course_modify_table)
+        self.course_modify_reset.clicked.connect()
+        self.course_modify_delete.clicked.connect()
+        self.course_modify_save.clicked.connect()
+
+    # For Values of Account page
+    def accountPage(self):
+        # show name, id, email
+        self.account_page_user_title.setText()
+        self.account_page_user_id.setText()
+        self.accoun_page_user_email.setText()
+
+        # show Credential Information
+        self.account_page_credential_reveal.clicked.connect()
+        self.account_page_credential_username.setText()
+        self.account_page_credential_password.setText()
+
+        # show Advisor Information
+        self.account_page_advisor_name.setText()
+        self.account_page_advisor_email.setText()
+
+        # Configure Your Account
+        self.config_account_name.text()
+        self.config_account_lastname.text()
+        self.config_account_email.text()
+        self.config_account_id.text()
+        self.config_account_username.text()
+        self.config_account_password.text()
+        self.config_account_advisor_name.text()
+        self.config_account_advisor_email.text()
+
+        # buttons
+        self.config_account_reset.clicked.connect()
+        self.config_account_save.clicked.connect()
+
 
 app = QApplication(sys.argv)
 user_interface = UserInterface()
 user_interface.show()
 app.exec()
->>>>>>> upstream/main
